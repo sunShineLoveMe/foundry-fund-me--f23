19346fba41757fdfddb5f0dc14b39fb6da3d973c
sunShineLoveMe <juneandrao@outlook.com>
2023-12-15 10:28:15 +0800

    add mocks aggregator

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import { Test, console } from "forge-std/Test.sol";
+import { FundMe } from "../src/FundMe.sol";
+import { DeployFundMe } from "../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        //  fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);   
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
+
+    function testMinimumDollarIsFive() public {
+       assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        console.log(fundMe.i_owner());
+        console.log(msg.sender);
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+}
